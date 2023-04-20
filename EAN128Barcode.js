906765489a3aac515bbb8780045eed0c2ed3d2c9
Andrey Kuzin <bentaxrus@mail.ru>
2023-04-20 17:30:26 +0300

    Create EAN128Barcode.js

---
+++ EAN128Barcode.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Barcode from "react-barcode";
+
+function EAN128Barcode() {
+  const [text, setText] = useState("");
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="textInput">Enter Text:</label>
+      <input id="textInput" type="text" value={text} onChange={handleInputChange} />
+      <br />
+      <Barcode value={text} format="EAN-128" />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default EAN128Barcode;
