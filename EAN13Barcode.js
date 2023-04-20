69f3053d6ea446cf673969733b52fc6b200053fa
Andrey Kuzin <bentaxrus@mail.ru>
2023-04-20 16:55:42 +0300

    Update EAN13Barcode.js

--- EAN13Barcode.js
+++ EAN13Barcode.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Barcode from "react-barcode";
 
-function EAN13Barcode({ digits }) {
-  // Add leading zeros to the digits string until it is 13 characters long
-  while (digits.length < 13) {
-    digits = "0" + digits;
-  }
+function EAN13Barcode() {
+  let [digits, setDigits] = useState("");
+
+  const handleInputChange = (event) => {
+    setDigits(event.target.value);
+  };
 
   return (
-    <Barcode
-      value={digits}
-      width={1.5}
-      height={50}
-      fontSize={14}
-      margin={5}
-      displayValue={false}
-      background="#ffffff"
-      lineColor="#000000"
-    />
+    <div>
+      <label htmlFor="digitsInput">Enter digits:</label>
+      <input
+        id="digitsInput"
+        type="text"
+        value={digits}
+        onChange={handleInputChange}
+      />
+      <br />
+      <Barcode
+        value={digits}
+        width={1.5}
+        height={50}
+        fontSize={14}
+        margin={5}
+        displayValue={false}
+        background="#ffffff"
+        lineColor="#000000"
+      />
+    </div>
   );
 }
 
