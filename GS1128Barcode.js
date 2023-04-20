bc83a7048d124a84722500bc13424139300f5344
Andrey Kuzin <bentaxrus@mail.ru>
2023-04-20 17:25:39 +0300

    Create GS1128Barcode.js

---
+++ GS1128Barcode.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Barcode from "react-barcode";
+
+function GS1128Barcode() {
+  const [text, setText] = useState("");
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="textInput">Enter Text:</label>
+      <input id="textInput" type="text" value={text} onChange={handleInputChange} />
+      <br />
+      <Barcode value={text} format="GS1-128" />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default GS1128Barcode;
