a41813cc9c615a0d1f64439d52d21711a2f4414a
Andrey Kuzin <bentaxrus@mail.ru>
2023-04-23 12:08:09 +0300

    Create QRcodeScripts.js

---
+++ QRcodeScripts.js
@@ -0,0 +1,5 @@
+// https://www.qrcode-tiger.com/?type=wifi
+
+// QR code data: WIFI:T:No encryption;S:HUAWEI-U5Q9;P:123456789;
+// QR code data: WIFI:T:WPA;S:HUAWEI-U5Q9;P:123456789;
+//QR code data: WIFI:T:WEP;S:HUAWEI-U5Q9;P:123456789;
